13641fcd27f269d98f747ba494226c0ade79116a
mz0018 <martinezhanzmenzi@gmail.com>
2025-05-09 13:29:43 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,11 +32,12 @@ app.use(cors({
     },
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
 
 // API routes
 app.use('/api', insert);
